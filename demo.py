4fa0adc9019b1760ce3a1cd5d75297bcfd8fb684
shashiDivs <shashipolity@gmail.com>
2023-03-12 00:07:31 +0530

    flasktest

---
+++ demo.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from visa.logger import logging
+
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET','POST'])
+def index():
+    logging.info("its testing")
+    return "Hello World"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
